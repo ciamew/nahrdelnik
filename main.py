6aa588ea411df972d4439dd78ff4aab8666595fe
ciamew <luciakotuc@gmail.com>
2023-05-30 21:56:31 +0200

    nieco nejde

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import random
 
 w = 500
 h = 500
-colours = ["pink", "yellow", "blue", "green"]
+colours = ["black", "lightblue", "blue", "purple"]
 count = 10
 size = 30
 move_list = []
 proces_list = []
-
+start = size
 def setup():
     global move_list
     for i in range(40):
@@ -25,6 +25,7 @@ def check_it(e):
             proces_list.append(zoz[0])
             move_list.remove(zoz[0])
             print("stalo sa")
+    move_it()
 
 def move_it():
     global proces_list
@@ -60,7 +61,34 @@ canvas.create_oval(12,466,20,474,fill="black")
 #         canvas.move(proces_list[0],20,470)
 #     canvas.after(10,slide_it)
 
+# def slide_it():
+#     global proces_list
+#     if len(proces_list) != 0:
+#         coord_ball = canvas.coords(proces_list[0])
+#         final_pos = (20,470)
+#         dx = w-coord_ball[2]
+#         dy = final_pos[1]-coord_ball[3]
+#         if dx>dy:
+#             dx = dx/dy
+#             dy = 1
+#         elif dx < dy:
+#             dy = dy / dx
+#             dx = 1
+#         canvas.move(proces_list[0],dx,dy)
+#         if coord_ball[2] == w:
+#             nit()
+#     canvas.after(10,slide_it)
 
+
+def nit():
+    global start, proces_list
+    coord_ball = canvas.coords(proces_list[0])
+    if coord_ball[0] > start:
+        canvas.move(proces_list[0],-1,0)
+        canvas.after(3,nit)
+    else:
+        start += size
+        proces_list = []
 
 
 
